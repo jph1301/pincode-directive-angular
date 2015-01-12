d1efb45e2a79a26de6399ce7d04ec20813063339
Zeeshan Hassan <zishon89us@hotmail.com>
2015-01-13 01:18:04 +0500

    refactored

--- app/js/dir.js
+++ app/js/dir.js
@@ -1,6 +1,6 @@
 myModule = angular.module("myModule", []);
-
-myModule.directive("pinCode", function () {
+myModule.constant("PASSWORD", "54541")
+myModule.directive("pinCode", function (PASSWORD) {
     return {
         restrict: 'E',
         templateUrl: '../directiveTemplate.html',
@@ -26,99 +26,80 @@ myModule.directive("pinCode", function () {
 
             */
             $scope.matchPassword = function () {
-               var url_to_get_password = 'password';
-                $http.post(url_to_get_password, {"password": $scope.selected.join("")}).success(function (data) {
 
-                    $scope.response = data.success;
-                }).then(function () {
-
-                    if ($scope.response) {
-
-                        $scope.message.type = 0;
-                        $scope.message.text = 'Success,Password Matched.';
-                        setTimeout(function () {
-                            $scope.clearAll();
-                            $scope.$digest();
-
-                        }, 2000);
-                    }
-
-                });
-
-
+                if($scope.selected.join("") == PASSWORD){
+                    return true;
+                }
 
             }
 
         },
-        link: function (scope, el, attr) {
-            $scope = scope;
-            $element = el;
-            scope.attempts = el.attr("attempts") || 3;
-            scope.howMany = el.attr("length") || 4;
+        link: function (scope, element, attr) {
+            scope.attempts = attr.attempts || 3;
+            scope.howMany = attr.length || PASSWORD.length || 4;
             scope.dots = [];
-            scope.loginTitle = el.attr("loginTitle") || 'My pin';
+            scope.loginTitle = element.attr("loginTitle") || 'My pin';
             for (a = 0; a < scope.howMany; a++) {
                 scope.dots.push(a);
             }
 
             /*  looking the screen when no of attempts exceeds                                        */
             scope.lockPin = function () {
-                el.find("ul").eq(1).css({display: "none"});
-                $scope.message = {};
-                $scope.message.type = 1;
-                $scope.message.text = 'Your phone is locked';
-                //$scope.$digest();
+                scope.message = {};
+                scope.message.type = 1;
+                scope.message.text = 'Your phone is locked';
+                //scope.$digest();
 
             };
 
 
             /* clearing the attempts(dots) / entered attempts */
-            $scope.clearAll = function () {
-                $scope.message = {};
-                $scope.selected = [];
-                el.find("ul").find("li").removeClass('filled');
+            scope.clearAll = function () {
+                scope.message = {};
+                scope.selected = [];
+                element.find("ul").find("li").removeClass('filled');
 
             };
 
             /* main function of this directing incrementing attempts checking for lenght etc and calling match password etc*/
-            $scope.selectDigit = function (num) {
-                if($scope.selected.length<$scope.howMany)
-                el.find("ul").find("li").eq($scope.selected.length).addClass('filled');
+            scope.selectDigit = function (num) {
+                if(!(scope.selected.length<scope.howMany)){
+                    return false;
+                }
 
-                $scope.selected.push(num);
-                $scope.matchPassword()
+                scope.selected.push(num);
 
+                if (scope.selected.length == scope.howMany) {
+                    scope.message = {};
 
-                $scope.howMany = $element.attr("length");
-
-                if ($scope.selected.length >= $scope.howMany) {
-                    $scope.message = {};
-
-                    if (!$scope.matchPassword()) {
-                        $scope.message.type = 1;
-                        //$scope.message.text = 'you cant enter more than ' + $scope.howMany + ' characters';
-                        $scope.message.text = 'Password Didint matched';
+                    if (!scope.matchPassword()) {
+                        scope.message.type = 1;
+                        //scope.message.text = 'you cant enter more than ' + scope.howMany + ' characters';
+                        scope.message.text = 'Password Didint matched';
 
                         setTimeout(function () {
-                            $scope.clearAll();
+                            scope.clearAll();
 
-                            console.log($scope.attempted);
-                            if ($scope.attempts == $scope.attempted) {
-                                $scope.lockPin();
-                            }
-                            $scope.attempted++;
-                            $scope.message = {};
-                            $scope.$digest();
+                            console.log(scope.attempted);
+                            scope.attempted++;
+                            scope.message = {};
+                            scope.$digest();
 
+                            (scope.attempts == scope.attempted) && (
+                                setTimeout(function(){
+                                    scope.attempted = 0;
+                                    scope.$digest();
+                                }, (attr.freezTime || 60) * 1000)
+                            )
                         }, 2000);
                     }
                     else {
 
-                        $scope.message.type = 0;
-                        $scope.message.text = 'Success, Password Matched.';
+                        scope.message.type = 0;
+                        scope.message.text = 'Success, Password Matched.';
                         setTimeout(function () {
-                            $scope.clearAll();
-                            $scope.$digest();
+                            scope.clearAll();
+                            scope.$digest();
 
                         }, 2000);
                     }
